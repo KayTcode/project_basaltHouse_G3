56fb1c6320c110869ab0a8adb8f3049feeda9d73
KayT <kayt1206.js@gmail.com>
2026-06-04 13:52:22 +0700

    feat: completed login with email and password

---
+++ basaltHouseSystem/src/java/dao/DBContext.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import utils.ConfigLoader;
+
+/**
+ *
+ * @author KayT
+ */
+public class DBContext {
+
+    protected Connection connection;
+
+    public DBContext() {
+        //@Students: You are not allowed to edit this method  
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String url = ConfigLoader.get("url");
+            String user = ConfigLoader.get("userID");
+            String pass = ConfigLoader.get("password");
+
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
